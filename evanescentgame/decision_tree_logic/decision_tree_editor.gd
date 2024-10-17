443b767092705e6845a79a063807cf6e4dca0f9c
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-10-17 04:48:47 -0700

    Changed node saving to resources

--- evanescentgame/decision_tree_logic/decision_tree_editor.gd
+++ evanescentgame/decision_tree_logic/decision_tree_editor.gd
@@ -2,7 +2,7 @@ extends Control
 
 const DECISION_TREE_NODE = preload("res://decision_tree_logic/decision_tree_graph/decision_tree_node.tscn")
 
-@onready var graph: GraphEdit = $GraphEdit
+@onready var graph: GraphEdit = $DecisionTreeGraph
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,20 +22,31 @@ func _notification(what):
 # Graph Node Logic
 
 func init_graph(graph_data: DecisionTreeGraphData):
-	pass
+	for node_data: DecisionTreeNodeData in graph_data.nodes:
+		var node = DECISION_TREE_NODE.instantiate()
+		node.init_node(node_data)
+		graph.add_child(node)
+	
+	for connection in graph_data.connections:
+		graph.connect_node(connection["from_node"], connection["from_port"], connection["to_node"], connection["to_port"])
 
 func add_new_node():
 	var node_scene = DECISION_TREE_NODE.instantiate()
 	graph.add_child(node_scene)
-	node_scene.owner = graph
 
 
+# Node Connection Requests
 
+func on_connection_request(from_node, from_slot, to_node, to_slot):
+	graph.connect_node(from_node, from_slot, to_node, to_slot)
+
+func on_disconnection_request(from_node, from_slot, to_node, to_slot):
+	graph.disconnect_node(from_node, from_slot, to_node, to_slot)
 
 # Graph Saving Logic
 
 func load_graph_data(file_name: String):
-	if ResourceLoader.exists(file_name):
+	if (ResourceLoader.exists(file_name)):
 		var graph_data = ResourceLoader.load(file_name)
 		if graph_data is DecisionTreeGraphData:
 			init_graph(graph_data)
@@ -48,15 +59,17 @@ func load_graph_data(file_name: String):
 
 func save_graph_data():
 	var graph_data = DecisionTreeGraphData.new()
-	graph_data.connections = $Graph.get_connection_list()
-	for node in $Graph.get_children():
-		if node is GraphNode:
+	graph_data.connections = graph.get_connection_list()
+	for node in graph.get_children():
+		if (node is GraphNode):
 			var node_data = DecisionTreeNodeData.new()
 			node_data.name = node.name
 			node_data.position_offset = node.position_offset
 			node_data.data = node.data
 			graph_data.nodes.append(node_data)
-	if ResourceSaver.save(graph_data, "res://decision_tree_logic/save_data/decision_tree_save_data.tres") == OK:
+	
+	var msg = ResourceSaver.save(graph_data, "res://decision_tree_logic/save_data/decision_tree_save_data.tres") 
+	if (msg == OK):
 		print("Saved Graph Data!")
 	else:
 		print("Error saving graph_data")
--- evanescentgame/decision_tree_logic/decision_tree_graph/decision_tree_node.gd
+++ evanescentgame/decision_tree_logic/decision_tree_graph/decision_tree_node.gd
@@ -1,14 +1,21 @@
 extends GraphNode
 
+enum SlotType {
+	DECISION = 0
+}
 
-# Called when the node enters the scene tree for the first time.
+var data
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	set_slot(0, true, SlotType.DECISION, Color.WHITE, true, SlotType.DECISION, Color.GREEN)
+	var titlebar = get_titlebar_hbox()
+	titlebar.remove_child(titlebar.get_child(0))
+	var text_edit = TextEdit.new()
+	text_edit.size_flags_horizontal = Control.SIZE_FILL
+	text_edit.size_flags_vertical = Control.SIZE_FILL
+	text_edit.custom_minimum_size = Vector2(size.x, 40)
+	titlebar.add_child(text_edit)
+	
 
 func _on_delete_request():
 	print("DELETING!")
@@ -16,3 +23,4 @@ func _on_delete_request():
 func init_node(node_data: DecisionTreeNodeData):
 	name = node_data.name
 	position_offset = node_data.position_offset
+	data = node_data.data
