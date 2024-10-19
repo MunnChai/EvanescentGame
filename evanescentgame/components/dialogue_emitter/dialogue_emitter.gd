9674bc10e058d6412bbc123db1f2e232534b3ff5
felixrl <felix.liu.ray@gmail.com>
2024-10-18 20:37:27 -0700

    Add custom dialogue box

--- evanescentgame/components/dialogue_emitter/dialogue_emitter.gd
+++ evanescentgame/components/dialogue_emitter/dialogue_emitter.gd
@@ -6,3 +6,4 @@ extends Node2D
 func show_dialogue(title: String):
 	player.is_input_active = false
 	DialogueManager.show_dialogue_balloon(dialogue_resource, title)
+ 
--- evanescentgame/instances/devil/devil.gd
+++ evanescentgame/instances/devil/devil.gd
@@ -1,19 +1,8 @@
 class_name Devil
-extends CharacterBody2D
+extends NPC
 
 ## Interaction behaviour for Lady Devil
 ## Mainly dialogue
-
-@onready var player: Player = get_tree().get_nodes_in_group("player")[0]
-@onready var interactable_area: InteractableArea = $InteractableArea
-
-signal signal_dialogue(title: String)
-
-func _ready():
-	pass
-
-func _physics_process(delta):
-	pass
 
 func on_player_interacted():
 	signal_dialogue.emit("talking_to_devil")
