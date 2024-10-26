a803a8b36f432e522efa51cb9069f08c7e9ed150
Ian Lee <ian.ant.lee@gmail.com>
2024-10-26 04:13:36 -0700

    added ui panel for changing locations (not implemented yet)

--- evanescentgame/components/location_exit/location_exit.gd
+++ evanescentgame/components/location_exit/location_exit.gd
@@ -1,11 +1,17 @@
 class_name LocationExit
 extends Node2D
 
+#@onready var nav_panel_scene: PackedScene = preload("res://ui/navigation_panel/navigation_panel.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+ 
+func _on_interactable_player_interacted():
+	get_tree().change_scene_to_file("res://ui/navigation_panel/navigation_panel.tscn")
---
+++ evanescentgame/ui/navigation_panel/navigation_panel.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# CHANGE SCENCES HERE
+
+func button1_pressed():
+	pass # Replace with function body.
+
+func button2_pressed():
+	pass # Replace with function body.
+
+func button3_pressed():
+	pass # Replace with function body.
