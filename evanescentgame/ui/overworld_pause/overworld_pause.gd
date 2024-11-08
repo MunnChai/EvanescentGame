933db66af03e0ef9c0b10bb9f12ed00806906bf4
Ian Lee <ian.ant.lee@gmail.com>
2024-11-08 15:23:08 -0800

    added placeholder options menu

--- evanescentgame/ui/overworld_pause/overworld_pause.gd
+++ evanescentgame/ui/overworld_pause/overworld_pause.gd
@@ -1,10 +1,13 @@
 extends Control
 
 @onready var animation_player = $AnimationPlayer
+@onready var options_menu = $OptionsMenu
+@onready var panel_container = $PanelContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.hide()
+	options_menu.hide()
+	#self.hide()
 	animation_player.play("RESET")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,27 +17,27 @@ func _process(delta):
 func resume():
 	get_tree().paused = false
 	animation_player.play_backwards("blur")
-	if !animation_player.is_playing():
-		self.hide()
 
 func pause():
 	get_tree().paused = true
-	self.show()
+	#self.show()
 	animation_player.play("blur")
 
 func escPressed():
-	if Input.is_action_just_pressed("pause") and !get_tree().paused:
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
 		pause()
-	elif Input.is_action_just_pressed("pause") and get_tree().paused:
+	elif Input.is_action_just_pressed("esc") and get_tree().paused and !options_menu.is_open:
 		resume()
 
 func _on_resume_pressed():
 	resume()
 
-# acts as a restart button for now cuz no options menu yet :(
 func _on_options_pressed():
-	get_tree().reload_current_scene()
-	resume()
+	options_menu.is_open = true
+	options_menu.show()
+	
+	options_menu.animation_player.play("slide_in")
+	animation_player.play("slide_out")
 
 func _on_quit_pressed():
 	resume()
