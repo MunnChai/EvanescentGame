8a087a6e938b16165681daffa9af781c9f5a7533
EricSong12345 <ericsong@Hotmail.ca>
2024-11-09 15:36:46 -0800

    evan house demo?

---
+++ evanescentgame/ui/navigation_panel/location_manager.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+signal player_exited
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_loc_1_player_exited():
+	player_exited.emit()
+
+func _on_loc_2_player_exited():
+	player_exited.emit()
+
+
+func _on_loc_3_player_exited():
+	player_exited.emit()
