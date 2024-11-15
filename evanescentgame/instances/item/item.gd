a32469fee51862a073fa01115324cfa6aff64f10
Ian Lee <ian.ant.lee@gmail.com>
2024-11-15 01:01:00 -0800

    clean up item script

--- evanescentgame/instances/item/item.gd
+++ evanescentgame/instances/item/item.gd
@@ -4,15 +4,12 @@ extends Node2D
 @onready var player: Player = get_tree().get_nodes_in_group("player")[0]
 @onready var interactable_area = $InteractableArea
 @onready var dialogue_emitter = $DialogueEmitter
-@onready var sprite = $Sprite2D
-
-@export var sprite_texture: Texture2D
+@onready var sprite_2d = $Sprite2D
 
 signal signal_dialogue(title: String)
 
 func _ready():
 	DialogueManager.dialogue_ended.connect(on_dialogue_ended)
-	sprite.texture = sprite_texture
 
 func on_player_interacted():
 	if (player.is_possessing):
@@ -34,4 +31,4 @@ func on_dialogue_ended(dialogue_resource: Resource):
 func pick_up():
 	interactable_area.disable()
 	player.currently_possessed_npc.add_to_inventory(self)
-	self.visible = false
+	sprite_2d.visible = false
