5b14d7a45a9c675dbff1f005d2c3ace5fdedadb1
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-11-16 16:08:42 -0800

    Changed resource to Control

--- evanescentgame/ui/Inventory/Inventory.gd
+++ evanescentgame/ui/Inventory/Inventory.gd
@@ -1,18 +1,20 @@
-extends Resource
+extends Control
 
-class_name Inv
+class_name Inventory
+
+@onready var slots: Array[Node] = get_node("MarginContainer/GridContainer").get_children()
+
+var items: Array[Item]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
 
 
-signal update
-@export var slots: Array[InvSlot]
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 
-func insert(item: InvItem):
-	var itemslots = slots.filter(func(slot): return slot.item == item)
-	if !itemslots.is_empty():
-		itemslots[0].amount += 1
-	else:
-		var emptyslots = slots.filter(func(slot): return slot.item == null)
-		if !emptyslots.is_empty():
-			emptyslots[0].item = item
-			emptyslots[0].anount = 1
-	update.emit()
+func update_slots():
+	for i in range(0, items.size()):
+		slots[i].get_node("CenterContainer/Panel/item_display").texture = items[i].sprite_2d.texture
