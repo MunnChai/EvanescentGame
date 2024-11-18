c84cbf16a0c9eab803d9ee7a27e7c5c6e5e09fd5
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-11-17 20:41:00 -0800

    Moved files into new directory structure

---
+++ evanescentgame/ui/main_menu/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+const PATH_TO_UNDERWORLD_INTRODUCTION = "res://scenes/underworld/underworld_introduction/underworld_introduction.tscn"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func start_game():
+	SceneLoader.load_scene(PATH_TO_UNDERWORLD_INTRODUCTION)
