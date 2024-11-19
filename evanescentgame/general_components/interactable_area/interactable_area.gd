f1ae16cae5402cff95a570e508877cbc6b808081
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-11-18 18:08:01 -0800

    Fixed interactable area overlap issue

--- evanescentgame/general_components/interactable_area/interactable_area.gd
+++ evanescentgame/general_components/interactable_area/interactable_area.gd
@@ -4,37 +4,49 @@ extends Area2D
 signal player_interacted
 
 @onready var player: Player = get_tree().get_nodes_in_group("player")[0]
+@onready var interact_symbol = $InteractSymbol
 
 var player_in: bool = false
 var is_enabled: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hide_interact_symbol()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	
+	if not is_enabled:
+		return
+
+func interact():
 	if not is_enabled:
 		return
 	
-	# Check player.is_input_active to prevent interactions when no input
-	if (player_in and player.is_input_active and Input.is_action_just_pressed("interact")):
-		player_interacted.emit()
+	player_interacted.emit()
 
 func enable():
 	is_enabled = true
 
 func disable():
 	is_enabled = false
-	$Label.visible = false
+	hide_interact_symbol()
 
 func _on_body_entered(body):
 	if (body is Player):
-		player_in = true
-		$Label.visible = true
+		body.add_interactable(self)
+		body.update_closest_interactable()
 
 
 func _on_body_exited(body):
 	if (body is Player):
-		player_in = false
-		$Label.visible = false
+		body.remove_interactable(self)
+		body.update_closest_interactable()
+		hide_interact_symbol()
+
+func show_interact_symbol():
+	if (is_enabled):
+		interact_symbol.show()
+
+func hide_interact_symbol():
+	interact_symbol.hide()
