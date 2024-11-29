5dcda42bb204453c002bb1a7a1606a2ce7d52d0a
alcme <alcmesloan@outlook.com>
2024-11-29 15:15:38 -0800

    added options menu
    created options menu w/ some basic functionality, also tweaked the main/pause menus slightly

--- evanescentgame/ui/main_menu/main_menu.gd
+++ evanescentgame/ui/main_menu/main_menu.gd
@@ -1,8 +1,11 @@
 extends Control
 
+@onready var options_menu = $OptionsMenu
+@onready var animation_player = $AnimationPlayer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	options_menu.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,3 +14,15 @@ func _process(delta):
 
 func start_game():
 	SceneLoader.load_underworld_introduction()
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_options_button_pressed():
+	options_menu.is_open = true
+	options_menu.show()
+
+	options_menu.animation_player.play("slide_in")
+	animation_player.play("slide_out")
