33ea9e1bb4193556a24165e3c4564547ae471362
felix liu <felix.liu.ray@gmail.com>
2024-11-30 15:09:26 -0800

    Refactor entry loading and info log script

--- evanescentgame/ui/player_interface/info_log/info_log.gd
+++ evanescentgame/ui/player_interface/info_log/info_log.gd
@@ -1,12 +1,7 @@
 class_name InfoLogLogic
 extends Control
 
-## STATE
-
-var is_open := false
-var current_entry: String = ""
-
-var tween: Tween = null
+#region UNLOCK FLAGS (STATIC)
 
 ## TRUE/FALSE FLAGS FOR CHECKING UNLOCKS
 ## Unlocks is STATIC since technically there is only
@@ -21,132 +16,136 @@ static func lock_flag(flag: String) -> void:
 	unlock_flags[flag] = false
 	SaveManager.save_game()
 
-## REFERENCES
-@onready var file_folder: AnimatedSprite2D = $FileFolder
+#endregion
 
-
-## PUBLIC METHODS
-
-## CALLED TO INITALIZE THE LOG
-## with unlocked entries, entry assets, etc.
-
-## Path to folder with all JSON files...
-const INFO_ENTRIES_PATH = "res://ui/player_interface/info_log/entries/"
-var json_entries = []
-var unlocked_json_entries = []
-
-func is_entry_unlocked(entry: Dictionary) -> bool:
-	var add := true
-	for flag in entry.get("unlock_flags", []):
-		if not is_unlocked(flag):
-			add = false
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	
-	#var strike_out := true
-	#for flag in entry.get("strike_out_flags", []):
-		#if not is_unlocked(flag):
-			#strike_out = false
-	
-	return add
-
-func init() -> void:
-	## NOTE: Probably need optimization around here during loading.
-	## Or load before start, and then select based on flags?
-	
-	var dir = DirAccess.open(INFO_ENTRIES_PATH)
-	if dir and json_entries.is_empty():
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			var file = FileAccess.open(INFO_ENTRIES_PATH + file_name, FileAccess.READ)
-			
-			var json = JSON.new()
-			
-			var data = json.parse(file.get_as_text())
-			
-			file.close()
-			
-			if not data == OK:
-				printerr("Issue with loading a JSON info entry!")
-				return
-			
-			json_entries.append(json.data)
-			if is_entry_unlocked(json.data):
-				unlocked_json_entries.append(json.data)
-			
-			file_name = dir.get_next()
+	_init()
 	
 	print(json_entries)
 
-const FADE_BG_TIME := 0.1
+func _init() -> void:
+	load_all_json_entries_from_dir()
 
-## Open to main screen...
-func open() -> void:
-	init()
+func _process(delta: float) -> void:
+	_process_animation()
 	
-	if tween != null:
-		tween.kill()
-	tween = create_tween()
-	tween.tween_property($BgPanel, "modulate", Color(0, 0, 0, 1), FADE_BG_TIME)
+	if Input.is_action_just_pressed("toggle_log"):
+		close() if is_open else open()
+
+#region ENTRY LOADING/PROCESSING
+
+## Path to folder with all JSON files...
+const INFO_ENTRIES_PATH = "res://ui/player_interface/info_log/entries/"
+## Path to node to instance for individual entries...
+const INFO_LOG_ENTRY = preload("info_log_entry.tscn")
+
+var json_entries: Array[InfoLogEntry] = []
+var unlocked_json_entries: Array[InfoLogEntry] = []
+
+func load_all_json_entries_from_dir() -> void:
+	var dir = DirAccess.open(INFO_ENTRIES_PATH)
+	
+	if not json_entries.is_empty(): # Never changes, only need to load once!
+		return
+	
+	if dir == null:
+		printerr("Error opening directory to load InfoLog JSON entries.")
+		return
+	
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		## FILE ACCESS
+		var file = FileAccess.open(INFO_ENTRIES_PATH + file_name, FileAccess.READ)
+		var json = JSON.new()
+		var error = json.parse(file.get_as_text())
+		file.close()
+		
+		if not error == OK:
+			printerr("Issue with loading a JSON info entry!")
+			return
+		
+		## CREATE NEW ENTRY
+		var new_entry: InfoLogEntry = INFO_LOG_ENTRY.instantiate()
+		new_entry.init_from_json(json.data)
+		add_child(new_entry)
+		
+		json_entries.append(new_entry)
+		if new_entry.is_unlocked():
+			unlocked_json_entries.append(new_entry)
+		
+		## Next!
+		file_name = dir.get_next()
+
+#endregion
+
+#region OPEN/CLOSING
+
+@onready var file_folder: AnimatedSprite2D = $FileFolder
+
+var is_open := false
+
+const FADE_BG_TIME := 0.1
+var bg_fade_tween: Tween = null
+
+func open() -> void:
+	## Fade in BG panel
+	_reset_tween()
+	bg_fade_tween = create_tween()
+	bg_fade_tween.tween_property($BgPanel, "modulate", Color(0, 0, 0, 1), FADE_BG_TIME)
+	
+	file_folder.play("file_flip_open")
 	
 	is_open = true
-	file_folder.play("file_flip_open")
-	print("Opened the info log!")
-	
 	get_tree().paused = true
-func open_to_main() -> void:
-	open()
 ## Open to a specific page or category, if it exists...
 func open_to_entry(entry_id: String) -> void:
 	open()
 	go_to_entry(entry_id)
 
-## Go to a specific page or category...
-func go_to_entry(entry_id: String) -> void:
-	if not is_unlocked(entry_id):
-		printerr("Attempted to go to an entry with ID " + str(entry_id) + " which has not yet been unlocked.")
-	## GOTO LOGIC
-func go_to_category(category_id: String) -> void:
-	pass
-
-## Close the info log
+## Close the info log.
 func close() -> void:
 	is_open = false
 	file_folder.play_backwards("file_flip_open")
-	print("Closed the info log!")
-	# $MarginContainer.hide()
 
-
-
-## DEBUG
-func _ready():
-	process_mode = Node.PROCESS_MODE_ALWAYS
-	$BgPanel.process_mode = Node.PROCESS_MODE_ALWAYS
-
+## SHOW the visual for the interior of the FILE FOLDER
+## when the right frame is hit.
 const FRAME_TO_SHOW := 13
 const FRAME_TO_HIDE := 12
-
-func _process(delta):
-	if Input.is_action_just_pressed("open_log"):
-		if is_open:
-			close()
-		else:
-			open()
-	
+func _process_animation() -> void:
 	if file_folder.frame == FRAME_TO_SHOW:
 		$Entries.show()
 	if file_folder.frame == FRAME_TO_HIDE:
 		$Entries.hide()
 
-
-
 func _on_file_folder_animation_finished():
 	if is_open:
-		# $MarginContainer.show()
-		pass
-	else:
-		if tween != null:
-			tween.kill()
-		tween = create_tween()
-		tween.tween_property($BgPanel, "modulate", Color(0, 0, 0, 0), FADE_BG_TIME)
-		get_tree().paused = false
+		return
+	
+	## LOG CLOSED, ANIMATION DONE.
+	_reset_tween()
+	bg_fade_tween = create_tween()
+	bg_fade_tween.tween_property($BgPanel, "modulate", Color(0, 0, 0, 0), FADE_BG_TIME)
+	
+	get_tree().paused = false
 
+func _reset_tween() -> void:
+	if bg_fade_tween != null:
+		bg_fade_tween.kill()
+
+#endregion
+
+#region NAVIGATION
+
+var current_entry: String = ""
+
+## Go to a specific page or category...
+func go_to_entry(entry_id: String) -> void:
+	pass
+	## GOTO LOGIC
+func go_to_category(category_id: String) -> void:
+	pass
+
+#endregion
--- evanescentgame/ui/player_interface/info_log/info_log_entry.gd
+++ evanescentgame/ui/player_interface/info_log/info_log_entry.gd
@@ -1,10 +1,67 @@
 class_name InfoLogEntry
 extends Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+## An instance of a InfoLog entry
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+## SAMPLE JSON
+#{
+  #"title": "Evan",	Name to show in ENTRY
+  #"sidebar_title": "Evan",	Name to show in SIDEBAR navigation
+  #“category”: “people”,	Name of the CATEGORY this entry belongs in 
+  #"description": "You?\nPresumably.",	Permanent description
+  #"unlock_flags": ["met_devil"],		Necessary flags to qualify for unlock
+  #"jot_notes": [
+	#{
+	  #"content": "Is a ghost.", 			Jot notes actual text
+	  #"unlock_flags": []				Jot note unlock requirements
+	#},
+	#{
+	  #"content": "Has a wife.",
+	  #"unlock_flags": ["met_wife"]
+	#}
+  #]
+#}
+
+func init_from_json(data: Dictionary) -> void:
+	title = data["title"]
+	sidebar_title = data["sidebar_title"]
+	category = data["category"]
+	description = data["description"]
+	unlock_flags = data["unlock_flags"]
+	
+	for jot_note in data["jot_notes"]:
+		var new_jot_note: JotNote = JotNote.new(jot_note["content"], jot_note["unlock_flags"])
+		jot_notes.append(new_jot_note)
+
+## Is THIS InfoLogEntry unlocked?
+func is_unlocked() -> bool:
+	var unlocked := true
+	for flag in unlock_flags:
+		if not InfoLogLogic.is_unlocked(flag):
+			unlocked = false
+	return unlocked
+
+
+
+var title: String = ""
+var sidebar_title: String = ""
+var category: String = ""
+var description: String = ""
+var unlock_flags: Array = [] # Of string...
+var jot_notes: Array[JotNote] = []
+
+class JotNote:
+	var content: String = ""
+	var unlock_flags: Array = [] # Of string...
+	
+	func _init(_content: String, _unlock_flags: Array) -> void:
+		content = _content
+		unlock_flags = _unlock_flags
+	
+	## Is THIS JotNote unlocked?
+	func is_unlocked() -> bool:
+		var unlocked := true
+		for flag in unlock_flags:
+			if not InfoLogLogic.is_unlocked(flag):
+				unlocked = false
+		return unlocked
