b957f39375fb7c42e4555b70bbed44f07a5e7fd7
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-02-22 15:17:40 -0800

    Added full dialogue skip

--- evanescentgame/ui/player_interface/dialogue_box/dialogue_box.gd
+++ evanescentgame/ui/player_interface/dialogue_box/dialogue_box.gd
@@ -231,6 +231,11 @@ func _on_mutated(_mutation: Dictionary) -> void:
 
 
 func _on_balloon_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_end"):
+		_hide_balloon()
+		DialogueManager.dialogue_ended.emit(resource)
+		return
+	
 	# See if we need to skip typing of the dialogue
 	if dialogue_label.is_typing:
 		var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
@@ -250,6 +255,9 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
 		next(dialogue_line.next_id)
 	elif event.is_action_pressed(next_action) and get_viewport().gui_get_focus_owner() == balloon:
 		next(dialogue_line.next_id)
+	 
+	
+
 
 
 func _on_responses_menu_response_selected(response: DialogueResponse) -> void:
